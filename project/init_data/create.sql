e2fd0436d105a6531d32f34ec809a8874d08a72a
LauraWhite-debug <70304709+LauraWhite-debug@users.noreply.github.com>
2021-10-29 12:00:20 -0600

    Add files via upload
    tables should populate

--- project/init_data/create.sql
+++ project/init_data/create.sql
@@ -2,10 +2,21 @@ DROP TABLE IF EXISTS ingredients CASCADE;
 CREATE TABLE IF NOT EXISTS ingredients(
   name VARCHAR(50) NOT NULL );
 
+DROP TABLE IF EXISTS recipes CASCADE;
+CREATE TABLE IF NOT EXISTS recipes(
+  dish_name VARCHAR(30),     
+  parts VARCHAR(30)[]);
 
 INSERT INTO ingredients(name)
 VALUES ('eggs'),('flour'),('butter'),('sugar'),('salt'),('pepper'),('baking powder'),
 ('milk'),('beef'),('pork'),('seafood'),('pasta'), ('parsley'),('onions'),('carrots'), ('potatos'),('vegatable oil'),
 ('garlic'),('lemon juice'), ('white wine'), ('cream'),('chili flakes'),('rice'),('soy sauce'), ('oyster sauce'),
 ('sesame oil') ;
+
+
+INSERT INTO recipes(dish_name, parts)
+VALUES('Muffins', ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs', 'milk', 'vegatable oil']),
+('Seafood Pasta', ARRAY ['seafood', 'garlic', 'lemon juice', 'white wine', 'cream', 'chili flakes', 'pasta']),
+('Fried Rice', ARRAY ['rice', 'soy sauce', 'oyster sauce', 'sesame oil', 'butter']),
+('Simple salad', ARRAY ['spinach', 'tomato', 'sesame oil']);
 
