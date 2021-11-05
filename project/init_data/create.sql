f755f398b67c39d0db560283f90d20410d2a8717
Laura White <laurawhite@Lauras-MacBook-Pro-2.local>
2021-11-05 16:47:48 -0600

    friday meeting

--- project/init_data/create.sql
+++ project/init_data/create.sql
@@ -5,7 +5,8 @@ CREATE TABLE IF NOT EXISTS ingredients(
 DROP TABLE IF EXISTS recipes CASCADE;
 CREATE TABLE IF NOT EXISTS recipes(
   dish_name VARCHAR(30), 
-  img VARCHAR(30),    
+  img VARCHAR(30),   
+  recipeInformation VARCHAR(6000),
   parts VARCHAR(30)[]);
 
 INSERT INTO ingredients(name)
@@ -15,13 +16,13 @@ VALUES ('eggs'),('flour'),('butter'),('sugar'),('salt'),('pepper'),('baking powd
 ('sesame oil') ;
 
 
-INSERT INTO recipes(dish_name, img, parts)
-VALUES('Muffins', '../views/img/cherryMuffins.png', ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs', 'milk', 'vegatable oil', 'cherries']),
-('Chocolate Chip Cookies', '../views/img/chocolateChipCookie.png', ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs', 'chocolate chips']),
-('Sugar Cookies', '../views/img/sugarCookies.png', ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs']),
-('Seafood Pasta', '../views/img/seafoodPasta.png', ARRAY ['shrimp', 'garlic', 'lemon juice', 'white wine', 'cream', 'chili flakes', 'pasta']),
-('Fried Rice', '../views/img/friedRice.png', ARRAY ['rice', 'egg', 'soy sauce', 'oyster sauce', 'sesame oil', 'butter']),
-('Simple salad','../views/img/simpleSalad.png', ARRAY ['spinach', 'tomato', 'red onion', 'sesame oil']);
+INSERT INTO recipes(dish_name, img, recipeInformation, parts)
+VALUES('Muffins', 'cherryMuffins.png', 'info', ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs', 'milk', 'vegatable oil', 'cherries']),
+('Chocolate Chip Cookies', 'chocolateChipCookie.png', 'info', ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs', 'chocolate chips']),
+('Sugar Cookies', 'sugarCookies.png', 'info', ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs']),
+('Seafood Pasta', 'seafoodPasta.png', 'info', ARRAY ['shrimp', 'garlic', 'lemon juice', 'white wine', 'cream', 'chili flakes', 'pasta']),
+('Fried Rice', 'friedRice.png', 'info', ARRAY ['rice', 'egg', 'soy sauce', 'oyster sauce', 'sesame oil', 'butter']),
+('Simple salad','simpleSalad.png', 'info', ARRAY ['spinach', 'tomato', 'red onion', 'sesame oil']);
 
 DROP TABLE IF EXISTS user_ingredients CASCADE;
 CREATE TABLE IF NOT EXISTS user_ingredients(
