39e064705cea70a4c73d31aeb6e11c3da53a9163
Laura White <laurawhite@Lauras-MacBook-Pro-2.local>
2021-11-12 09:56:39 -0700

    during lab

--- project/init_data/create.sql
+++ project/init_data/create.sql
@@ -6,11 +6,9 @@ DROP TABLE IF EXISTS recipes CASCADE;
 CREATE TABLE IF NOT EXISTS recipes(
   dish_name VARCHAR(30), 
   img VARCHAR(30),   
-  recipeInformation VARCHAR(6000),
+  recipeBlurb VARCHAR(6000),
   quantity VARCHAR(3000)[],
-  parts VARCHAR(30)[],
-  recipeBlurb VARCHAR(900)
-  );
+  parts VARCHAR(30)[]);
 
 
 INSERT INTO ingredients(name)
@@ -29,14 +27,14 @@ VALUES('Muffins', 'cherryMuffins.png', ARRAY ['put recipe measurements here'], A
 ('Simple salad','simpleSalad.png', ARRAY ['put recipe measurements here'], ARRAY ['spinach', 'tomato', 'red onion', 'sesame oil'], 'recipe blurb here'),
 ('Tom Yum Soup', 'tomYum.jpeg', ARRAY ['put recipe measurements here'], ARRAY ['shrimp', 'garlic', 'chicken stock','mushroom','tomato','onion'], 'recipe blurb here'),
 ('ERROR! You didnt give green machine any ingredients to work with!','error.png', ARRAY [''], ARRAY[''],'Click go back to home!');*/
-INSERT INTO recipes(dish_name, img, recipeInformation, parts)
-VALUES('Muffins', 'cherryMuffins.png', 'info', ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs', 'milk', 'vegatable oil', 'cherries']),
-('Chocolate Chip Cookies', 'chocolateChipCookie.png', 'info', ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs', 'chocolate chips']),
-('Sugar Cookies', 'sugarCookies.png', 'info',  ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs']),
-('Seafood Pasta', 'seafoodPasta.png', 'info', ARRAY ['shrimp', 'garlic', 'lemon juice', 'white wine', 'cream', 'chili flakes', 'pasta']),
-('Fried Rice', 'friedRice.png', 'info', ARRAY ['rice', 'egg', 'soy sauce', 'oyster sauce', 'sesame oil', 'butter']),
-('Simple salad','simpleSalad.png', 'info', ARRAY ['spinach', 'tomato', 'red onion', 'sesame oil']),
-('Tom Yum Soup', 'tomYum.jpeg', 'info', ARRAY ['shrimp', 'garlic', 'chicken stock','mushroom','tomato','onion']);
+INSERT INTO recipes(dish_name, img, recipeBlurb, quantity, parts)
+VALUES('Muffins', 'cherryMuffins.png', 'recipeBlurb', ARRAY ['1 cup flour', '1/2 sugar'], ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs', 'milk', 'vegatable oil', 'cherries']),
+('Chocolate Chip Cookies', 'chocolateChipCookie.png', 'recipeBlurb', ARRAY ['1 cup flour', '1/2 sugar'], ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs', 'chocolate chips']),
+('Sugar Cookies', 'sugarCookies.png', 'recipeBlurb',  ARRAY ['1 cup flour', '1/2 sugar'], ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs']),
+('Seafood Pasta', 'seafoodPasta.png', 'recipeBlurb', ARRAY ['1 cup flour', '1/2 sugar'], ARRAY ['shrimp', 'garlic', 'lemon juice', 'white wine', 'cream', 'chili flakes', 'pasta']),
+('Fried Rice', 'friedRice.png', 'recipeBlurb', ARRAY ['1 cup flour', '1/2 sugar'], ARRAY ['rice', 'egg', 'soy sauce', 'oyster sauce', 'sesame oil', 'butter']),
+('Simple salad','simpleSalad.png', 'recipeBlurb', ARRAY ['1 cup flour', '1/2 sugar'], ARRAY ['spinach', 'tomato', 'red onion', 'sesame oil']),
+('Tom Yum Soup', 'tomYum.jpeg', 'recipeBlurb', ARRAY ['1 cup flour', '1/2 sugar'], ARRAY ['shrimp', 'garlic', 'chicken stock','mushroom','tomato','onion']);
 
 DROP TABLE IF EXISTS user_ingredients CASCADE;
 CREATE TABLE IF NOT EXISTS user_ingredients(
