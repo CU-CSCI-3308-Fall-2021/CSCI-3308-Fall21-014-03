31501f1bc2afc86bae7511a584285afa0761084f
zionwashington <zion.washington@colorado.edu>
2021-11-12 09:22:17 -0700

    commented out code

--- project/init_data/create.sql
+++ project/init_data/create.sql
@@ -4,11 +4,13 @@ CREATE TABLE IF NOT EXISTS ingredients(
 
 DROP TABLE IF EXISTS recipes CASCADE;
 CREATE TABLE IF NOT EXISTS recipes(
-  dish_name VARCHAR(300), 
+  dish_name VARCHAR(30), 
   img VARCHAR(30),   
   recipeInformation VARCHAR(6000),
-  quantity VARCHAR(30[]),
-  parts VARCHAR(30)[]);
+  quantity VARCHAR(3000)[],
+  parts VARCHAR(30)[],
+  recipeBlurb VARCHAR(900)
+  );
 
 
 INSERT INTO ingredients(name)
@@ -18,17 +20,27 @@ VALUES ('eggs'),('flour'),('butter'),('sugar'),('salt'),('pepper'),('baking powd
 ('sesame oil') ;
 
 
-INSERT INTO recipes(dish_name, img, recipeInformation, quantity, parts)
-VALUES('Muffins', 'cherryMuffins.png', 'info', ARRAY['test'], ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs', 'milk', 'vegatable oil', 'cherries']),
+/*INSERT INTO recipes(dish_name, img, recipeInformation, parts, recipeBlurb)
+VALUES('Muffins', 'cherryMuffins.png', ARRAY ['put recipe measurements here'], ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs', 'milk', 'vegatable oil', 'cherries'], 'recipe blurb here'),
+('Chocolate Chip Cookies', 'chocolateChipCookie.png', ARRAY ['put recipe measurements here'], ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs', 'chocolate chips'], 'recipe blurb here'),
+('Sugar Cookies', 'sugarCookies.png', ARRAY ['put recipe measurements here'],  ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs'], 'recipe blurb here'),
+('Seafood Pasta', 'seafoodPasta.png', ARRAY ['put recipe measurements here'], ARRAY ['shrimp', 'garlic', 'lemon juice', 'white wine', 'cream', 'chili flakes', 'pasta'], 'recipe blurb here'),
+('Fried Rice', 'friedRice.png', ARRAY ['put recipe measurements here'], ARRAY ['rice', 'egg', 'soy sauce', 'oyster sauce', 'sesame oil', 'butter'], 'recipe blurb here'),
+('Simple salad','simpleSalad.png', ARRAY ['put recipe measurements here'], ARRAY ['spinach', 'tomato', 'red onion', 'sesame oil'], 'recipe blurb here'),
+('Tom Yum Soup', 'tomYum.jpeg', ARRAY ['put recipe measurements here'], ARRAY ['shrimp', 'garlic', 'chicken stock','mushroom','tomato','onion'], 'recipe blurb here'),
+('ERROR! You didnt give green machine any ingredients to work with!','error.png', ARRAY [''], ARRAY[''],'Click go back to home!');*/
+INSERT INTO recipes(dish_name, img, recipeInformation, parts)
+VALUES('Muffins', 'cherryMuffins.png', 'info', ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs', 'milk', 'vegatable oil', 'cherries']),
 ('Chocolate Chip Cookies', 'chocolateChipCookie.png', 'info', ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs', 'chocolate chips']),
 ('Sugar Cookies', 'sugarCookies.png', 'info',  ARRAY ['flour', 'baking powder', 'salt', 'sugar', 'eggs']),
 ('Seafood Pasta', 'seafoodPasta.png', 'info', ARRAY ['shrimp', 'garlic', 'lemon juice', 'white wine', 'cream', 'chili flakes', 'pasta']),
 ('Fried Rice', 'friedRice.png', 'info', ARRAY ['rice', 'egg', 'soy sauce', 'oyster sauce', 'sesame oil', 'butter']),
 ('Simple salad','simpleSalad.png', 'info', ARRAY ['spinach', 'tomato', 'red onion', 'sesame oil']),
-('Tom Yum Soup', 'tomYum.jpeg', 'info', ARRAY ['shrimp', 'garlic', 'chicken stock','mushroom','tomato','onion']),
-('ERROR! You didnt give green machine any ingredients to work with!','error.jpg','info',ARRAY[]);
+('Tom Yum Soup', 'tomYum.jpeg', 'info', ARRAY ['shrimp', 'garlic', 'chicken stock','mushroom','tomato','onion']);
 
 DROP TABLE IF EXISTS user_ingredients CASCADE;
 CREATE TABLE IF NOT EXISTS user_ingredients(
   ingredient_name VARCHAR(15));
+
+  
 
